ca1e6ec8da5e17f1e9cdc3152b0327226ce1bfd2
AmriteshRaj123 <pathakamritesh6@gmail.com>
2025-03-18 08:12:29 +0530

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="fixed bottom-0 md:top-0 w-full bg-white border-t md:border-b border-gray-200 px-4 py-3 z-50">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
         <Link to="/" className="hidden md:block text-xl font-semibold">
-          Instagram
+          Instavibe
         </Link>
         
         <div className="flex items-center justify-around md:justify-end w-full md:w-auto space-x-6">
